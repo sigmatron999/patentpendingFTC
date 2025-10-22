a6e5f82ace2fef38729df8645049055e8dfaa82a
kaavyad25 <kaavyadev25@gmail.com>
2025-10-21 21:58:07 -0600

    Update DriveTrain.java

--- DriveTrain.java
+++ DriveTrain.java
@@ -10,11 +10,13 @@ import com.qualcomm.robotcore.util.Range;
 
 public class DriveTrain {
     private double speedScalar = 1.0;
+
+    // initial motors
     private DcMotor frontLeftMotor = null;
     private DcMotor frontRightMotor = null;
     private DcMotor backLeftMotor = null;
     private DcMotor backRightMotor = null;
-
+    
     public boolean isMoving = true;
 
 
@@ -35,6 +37,12 @@ public class DriveTrain {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+
+        /* -----------------------------------------
+        IF THE ROBOT IS DRIVING IN REVERSE, FLIP THESE
+        ---------------------------------------------
+        */
+        
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
         backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
 
@@ -44,6 +52,8 @@ public class DriveTrain {
 
     }
 
+    // restrict the angle reading, in radians, from the imu [-π, π]
+    
     private double angleWrap(double rad){
 
         while (rad > Math.PI){
@@ -57,30 +67,46 @@ public class DriveTrain {
         return rad;
     }
 
+
+    /*
+    field oriented drive; movement control is relative to the field, disregarding robot direction
+    ex. If the robot is facing backward relative to the field, but the joystick points forward, the robot drives forward
+    */
+    
     public void fieldOrientedTranslate(double targetPowerX, double targetPowerY, double rotation, double currentRotation) {
 
+
+        // direction robot is facing, in angles
         double robotYawDeg = Math.toDegrees(angleWrap(Math.toRadians(currentRotation)));
 
+        // direction stick is pointing, mapped to [-180, 180]
         double stickRotationDeg = Math.atan2(targetPowerY, targetPowerX) * 180/Math.PI;
 
+        // offset joystick vector to account for robot orientation
         double thetaDeg = 360.0 - robotYawDeg + stickRotationDeg;
-
-
+        
+        // sets power to be length of joystick vector
         double power = Math.hypot(targetPowerX, targetPowerY);
 
+        // restricting power between [-1, 1] because of a bug;
+        // if abs(targetPowerX), abs(targetPowerY) = 1, then power would be abs(√2)
         power = Range.clip(power, -1.0, 1.0);
 
+
+        // sin, cos of corrected angle
         double sin = Math.sin((thetaDeg*Math.PI/180.0) + Math.PI/4);
         double cos = Math.cos((thetaDeg*Math.PI/180.0) + Math.PI/4);
 
+        // max of sin, cos; prevents sin, cos from exceding 1
         double maxSinCos = Math.max(Math.abs(sin), Math.abs(cos));
 
+        
         double frontRightPower;
         double frontLeftPower;
         double backRightPower;
         double backLeftPower;
 
-
+        
         rotation *= -1;
 
         frontLeftPower = power*cos/maxSinCos + rotation;
@@ -93,7 +119,7 @@ public class DriveTrain {
         double backMax = Math.max(Math.abs(backLeftPower), Math.abs(backRightPower));
 
 
-
+        // a normalization
         if ((power + Math.abs(rotation) > 1)){
 
 
@@ -108,6 +134,7 @@ public class DriveTrain {
 
             if (motorsNotNull()) {
 
+                // speedScalar will be set from the main function (whenever its built)
                 frontLeftMotor.setPower(frontLeftPower * speedScalar);
                 frontRightMotor.setPower(frontRightPower * speedScalar);
 
@@ -118,9 +145,13 @@ public class DriveTrain {
         }
         
     }
+    // android studio was complaining about the complex if statement
     private boolean motorsNotNull() {
         return frontLeftMotor != null && frontRightMotor != null && backLeftMotor != null && backRightMotor != null;
     }
+
+    // drive relative to the robot
+    // ex. forward on joystick = forward on robot, even if the robot is sideways
     public void robotOrientedTranslate(double targetPowerX, double targetPowerY, double rotation){
 
         double thetaRad = Math.atan2(targetPowerY, targetPowerX);
@@ -145,7 +176,7 @@ public class DriveTrain {
         frontRightPower = power*cos/maxSinCos - rotation;
         backRightPower = power*sin/maxSinCos - rotation;
 
-
+        
         if ((1.0 < power + Math.abs(rotation))){
 
 
@@ -158,9 +189,7 @@ public class DriveTrain {
 
         if (isMoving){
 
-            if (frontLeftMotor != null && frontRightMotor != null && backLeftMotor != null && backRightMotor != null) {
-
-
+            if (motorsNotNull()) {
 
                 frontLeftMotor.setPower(frontLeftPower * speedScalar);
                 frontRightMotor.setPower(frontRightPower * speedScalar);
@@ -175,7 +204,7 @@ public class DriveTrain {
 
     public void setSpeedScalar(double change)
     {
-        //sets the speed scalar of the motors to a specified value in starter bot
+        // asssigned from main function (whenever we build it)
         speedScalar = change;
 
     }
